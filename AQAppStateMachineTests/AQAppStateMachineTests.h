0a28919845e6a26600f5287375a58a8c15532cae
Jim Dovey <jdovey@kobobooks.com>
2011-06-14 12:23:05 -0400

    Initial Commit

---
+++ AQAppStateMachineTests/AQAppStateMachineTests.h
@@ -0,0 +1,13 @@
+//
+//  AQAppStateMachineTests.h
+//  AQAppStateMachineTests
+//
+//  Created by Jim Dovey on 11-06-14.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AQAppStateMachineTests : SenTestCase
+
+@end
---
+++ AQAppStateMachineTests/AQAppStateMachineTests.m
@@ -0,0 +1,32 @@
+//
+//  AQAppStateMachineTests.m
+//  AQAppStateMachineTests
+//
+//  Created by Jim Dovey on 11-06-14.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AQAppStateMachineTests.h"
+
+@implementation AQAppStateMachineTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AQAppStateMachineTests");
+}
+
+@end
